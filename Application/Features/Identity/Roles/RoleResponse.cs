1e3675233f8c71de1abf1133382cda793239257d
Guilherme da Costa de Albuquerque <guicalbuquerque@gmail.com>
2025-08-04 00:20:11 -0300

    fix typo

--- Application/Features/Identity/Roles/RoleResponse.cs
+++ Application/Features/Identity/Roles/RoleResponse.cs
@@ -3,7 +3,7 @@
 public class RoleResponse
 {
     public string Id { get; set; }
-    public string Namme { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
     public List<string> Permissions { get; set; } = [];
 }
